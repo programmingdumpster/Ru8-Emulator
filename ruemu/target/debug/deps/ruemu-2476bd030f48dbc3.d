828f079c58bac5a15a6843ef9d5464d581df359f
programmingdumpster <Mateusz.S-ki@proton.me>
2026-03-31 22:00:28 +0200

    Added .gitignore

---
+++ ruemu/target/debug/deps/ruemu-2476bd030f48dbc3.d
@@ -0,0 +1,5 @@
+C:\Users\Matt\Documents\Ru8-Emulator\ruemu\target\debug\deps\ruemu-2476bd030f48dbc3.d: src\main.rs
+
+C:\Users\Matt\Documents\Ru8-Emulator\ruemu\target\debug\deps\libruemu-2476bd030f48dbc3.rmeta: src\main.rs
+
+src\main.rs:
---
+++ ruemu/target/debug/deps/ruemu-8d3fdad69d0185c2.d
@@ -0,0 +1,5 @@
+C:\Users\Matt\Documents\Ru8-Emulator\ruemu\target\debug\deps\ruemu-8d3fdad69d0185c2.d: src\main.rs
+
+C:\Users\Matt\Documents\Ru8-Emulator\ruemu\target\debug\deps\libruemu-8d3fdad69d0185c2.rmeta: src\main.rs
+
+src\main.rs:
